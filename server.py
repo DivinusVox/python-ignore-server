8fdd5cf1300b8e1a8d3449f80e4b8f3dd4bad175
DivinusVox <cole.kacey@gmail.com>
2013-08-06 00:14:30 -0600

    Modularized, future-proofed, and better error handling

--- server.py
+++ server.py
@@ -2,27 +2,35 @@
 import argparse
 import socket
 
-parser = argparse.ArgumentParser(description='Basic socket server which \
-        ignores traffic on the specified port.')
-parser.add_argument('--port', '-p',
-                    metavar='port',
-                    required=True,
-                    type=int,
-                    help='int specifying binding port')
+def main():
+    parser = argparse.ArgumentParser(description='Basic socket server which \
+            ignores traffic on the specified port.')
+    parser.add_argument('--port', '-p',
+                        metavar='port',
+                        required=True,
+                        type=int,
+                        help='int specifying binding port')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-try:
-    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('', args.port))
-    s.listen(5)
-except socket.error:
-    print 'Failed to bind to socket for port {}'.format(args.port)
-    print 'Try:\n sudo ./server.py -p {}'.format(args.port)
-    exit()
+    try:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.bind(('', args.port))
+        s.listen(5)
+    except socket.error:
+        print('Failed to bind to socket for port {}.'.format(args.port))
+        if int(args.port) < 1025:  # privileged ports:
+            print('This is a privileged port, try:\n sudo ./server.py \
+                    -p {}'.format(args.port))
+        print('Ensure the port is not currently in use by another program.')
+        exit()
 
-print '-- Now ignoring on port {}...'.format(args.port)
+    print('-- Now ignoring on port {}...'.format(args.port))
 
-while True:
-    conn, address = s.accept()
-    conn.close()
+    while True:
+        conn, address = s.accept()
+        conn.close()
+
+
+if __name__ == '__main__':
+    main()
