0de21cc58d57a7074508dafbcb3bc170358b79fb
DivinusVox <cole.kacey@gmail.com>
2013-08-06 01:28:13 -0600

    Arg handlingi n SocketServer implementation

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python
+import argparse
+import socket
 import SocketServer
 
 class IgnoreHandler(SocketServer.BaseRequestHandler):
@@ -8,9 +10,30 @@ class IgnoreHandler(SocketServer.BaseRequestHandler):
     def handle(self):
         self.request.close()
 
-if __name__ == '__main__':
-    HOST = ''
-    PORT = 9999
 
-    server = SocketServer.TCPServer((HOST, PORT), IgnoreHandler)
+def main():
+    parser = argparse.ArgumentParser(
+            description="Basic socket server which ignores traffic on the \
+            specified port.")
+    parser.add_argument('--port', '-p',
+                        metavar='port',
+                        required=True,
+                        type=int,
+                        help='Numeral value of the port to bind')
+    args = parser.parse_args()
+
+    HOST = ''
+    PORT = args.port
+
+    try:
+        server = SocketServer.TCPServer((HOST, PORT), IgnoreHandler)
+    except socket.error as e:
+        print('Unable to bind socket {}'.format(PORT))
+        print('ERROR: {}'.format(e[1]))
+        exit()
+
     server.serve_forever()
+
+
+if __name__ == '__main__':
+    main()
