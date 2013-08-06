4877365dbb5a659384f5012b17e8f5fd7d720cca
DivinusVox <cole.kacey@gmail.com>
2013-08-06 01:09:01 -0600

    Moved raw handler in favor of Server class

--- server.py
+++ server.py
@@ -1,37 +1,16 @@
 #!/usr/bin/env python
-import argparse
-import socket
+import SocketServer
 
-def main():
-    parser = argparse.ArgumentParser(
-        description="Basic socket server which ignores traffic on the \
-        specified port.")
-    parser.add_argument('--port', '-p',
-                        metavar='port',
-                        required=True,
-                        type=int,
-                        help='int specifying binding port')
-
-    args = parser.parse_args()
-
-    try:
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.bind(('', args.port))  # listen on all bound IPv4 addresses
-        s.listen(5)  # 5 tends to be default max queue in *nix
-    except socket.error:
-        print('Failed to bind to socket for port {}.'.format(args.port))
-        if int(args.port) < 1024:  # privileged ports:
-            print("This is a privileged port, try:")
-            print("  > sudo ./server.py -p {}".format(args.port))
-        print('Ensure the port is not currently in use by another program.')
-        exit()
-
-    print('-- Now ignoring on port {}...'.format(args.port))
-
-    while True:
-        conn, address = s.accept()
-        conn.close()
-
+class IgnoreHandler(SocketServer.BaseRequestHandler):
+    """
+    Handler to ignore incoming requests.
+    """
+    def handle(self):
+        self.request.close()
 
 if __name__ == '__main__':
-    main()
+    HOST = ''
+    PORT = 9999
+
+    server = SocketServer.TCPServer((HOST, PORT), IgnoreHandler)
+    server.serve_forever()
