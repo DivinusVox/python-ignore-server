e5f4f571402983c672ef6f73e4d7a4eaab86cd4c
DivinusVox <cole.kacey@gmail.com>
2013-08-06 00:00:05 -0600

    Added port argument and templated output

--- server.py
+++ server.py
@@ -1,15 +1,27 @@
-#/usr/bin/env python
+#!/usr/bin/env python
+import argparse
 import socket
+
+parser = argparse.ArgumentParser(description='Basic socket server which \
+        ignores traffic on the specified port.')
+parser.add_argument('--port', '-p',
+                    metavar='port',
+                    required=True,
+                    type=int,
+                    help='int specifying binding port')
+
+args = parser.parse_args()
 
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(('', 443))
+    s.bind(('', args.port))
     s.listen(5)
 except socket.error:
-    print 'Failed to bind to socket for port 443'
+    print 'Failed to bind to socket for port {}'.format(args.port)
+    print 'Try:\n sudo ./server.py -p {}'.format(args.port)
     exit()
 
-print '-- Now ignoring on port 443...'
+print '-- Now ignoring on port {}...'.format(args.port)
 
 while True:
     conn, address = s.accept()
